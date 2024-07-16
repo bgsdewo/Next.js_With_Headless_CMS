483be0a2fa6299add613b88069b42921571dae76
unknown <bgsdewoo20@gmail.com>
2024-07-16 13:56:02 +0700

    Menggunakan Component Link Untuk Tautan

--- learn-nextjs/app/layout.jsx
+++ learn-nextjs/app/layout.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <html lang="en">
@@ -5,7 +6,24 @@ export default function Layout({ children }) {
         <title>LearnNextJs</title>
       </head>
       <body>
-        <header>[header]</header>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <main>{children}</main>
         <footer>[footer]</footer>
       </body>
