bc7dbb0c54822a29fa2318ba884d445a175bfe04
unknown <bgsdewoo20@gmail.com>
2024-07-16 11:13:04 +0700

    Perbedaan Server Component Dan Client Component

--- learn-nextjs/app/page.jsx
+++ learn-nextjs/app/page.jsx
@@ -1,5 +1,11 @@
+//karena nextjs app ini berbasis server tapi jika ingin
+//mengubahnya ke client side maka cukup tambahkan
+"use client";
+import { useEffect } from "react";
 export default function Home() {
-  console.log("hello world");
+  useEffect(() => {
+    window.alert("Hello world");
+  }, []);
   return (
     <>
       <h1>My Next.js App</h1>
