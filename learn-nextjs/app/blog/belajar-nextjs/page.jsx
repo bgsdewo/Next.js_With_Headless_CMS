b238c949df5c8a25f6800e0d733867a8d3d4c9d8
unknown <bgsdewoo20@gmail.com>
2024-07-17 13:12:38 +0700

    Menggunakan Static Assets

--- learn-nextjs/app/blog/belajar-nextjs/page.jsx
+++ learn-nextjs/app/blog/belajar-nextjs/page.jsx
@@ -4,6 +4,13 @@ export default function PostPage() {
   return (
     <>
       <Heading>Belajar Next Js</Heading>
+      <img
+        src="/images/image-1.jpg"
+        alt=""
+        width={640}
+        height={360}
+        className="mb-2 rounded"
+      />
       <p>Halaman dari Blog</p>
     </>
   );
--- learn-nextjs/app/blog/latihan-route-nextjs/page.jsx
+++ learn-nextjs/app/blog/latihan-route-nextjs/page.jsx
@@ -4,6 +4,13 @@ export default function PostPage() {
   return (
     <>
       <Heading>latihan route next js</Heading>
+      <img
+        src="/images/image-2.jpg"
+        alt=""
+        width={640}
+        height={360}
+        className="mb-2 rounded"
+      />
       <p>Halaman dari Blog</p>
     </>
   );
