c834013b317d624a1334ae1ef8af78b2e311cd4f
unknown <bgsdewoo20@gmail.com>
2024-07-17 11:42:08 +0700

    Memanfaatkan Color Pallete Tailwindcss

--- learn-nextjs/app/layout.jsx
+++ learn-nextjs/app/layout.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Navbar from "../components/Navbar";
 import "./globals.css";
 export default function Layout({ children }) {
   return (
@@ -6,26 +6,9 @@ export default function Layout({ children }) {
       <head>
         <title>LearnNextJs</title>
       </head>
-      <body className="flex flex-col px-4 py-4 min-h-screen">
+      <body className="bg-gray-100 flex flex-col px-4 py-4 min-h-screen">
         <header>
-          <nav>
-            <ul className="flex gap-2">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/blog">Blog</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-              <li>
-                <Link href="/contact" prefetch={false}>
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <Navbar />
         </header>
         <main className="py-3 grow">{children}</main>
         <footer className="border-t py-3 text-center text-xs">
