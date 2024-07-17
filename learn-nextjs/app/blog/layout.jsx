5222e5b4787196b5e7c5df511197637d24a0b195
unknown <bgsdewoo20@gmail.com>
2024-07-17 21:05:15 +0700

    Membuat Card Post List

--- learn-nextjs/app/blog/layout.jsx
+++ learn-nextjs/app/blog/layout.jsx
@@ -1,8 +1,8 @@
 export default function BlogLayout({ children }) {
   return (
-    <div style={{ display: "flex" }}>
-      <div style={{ border: "1px solid black" }}>Side barr</div>
-      <div>{children}</div>
+    <div className="flex">
+      <div className="">[SideBar]</div>
+      <div className="px-4">{children}</div>
     </div>
   );
 }
