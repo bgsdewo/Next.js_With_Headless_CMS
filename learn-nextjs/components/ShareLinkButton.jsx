1b7b8e6bce00c4630de8a95261f0d693283695b5
unknown <bgsdewoo20@gmail.com>
2024-07-20 10:03:47 +0700

    Install Hero Icon di github dan Percantik Halaman Web Dengan Icon Berbasis Tailwindcss

--- learn-nextjs/components/ShareLinkButton.jsx
+++ learn-nextjs/components/ShareLinkButton.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { LinkIcon } from "@heroicons/react/20/solid";
 import { useState } from "react";
 
 export default function ShareLinkButton() {
@@ -12,8 +13,9 @@ export default function ShareLinkButton() {
   return (
     <button
       onClick={handleClick}
-      className="border px-2 py-1 rounded text-gray-500 text-sm hover:bg-gray-200 hover:text-gray-700"
+      className="border  flex gap-1 items-center px-2 py-1 rounded text-gray-500 text-sm hover:bg-gray-200 hover:text-gray-700"
     >
+      <LinkIcon className="h-4 w-4" />
       {copied ? "Link Copied" : "Share Link"}
     </button>
   );
